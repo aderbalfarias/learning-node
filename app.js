a48d2934715535b9606e698b1e2be8fbe14a2061
Aderbal Farias <aderbalfarias@hotmail.com>
2018-08-02 14:49:46 -0300

    Adding examples

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 console.log("fs example");
 
 var fs = require('fs');
+const notes = require('./notes.js');
+
+console.log('Result:', notes.add(9, -2));
+console.log(notes.addNote);
 
 fs.appendFile('Files/HelloFile.txt', 'Hello content!', function (err) {
     if (err) throw err;
     console.log('Saved!');
 });
+
