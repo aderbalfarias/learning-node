6ae7e9b9c3639e2f7e5b5f832dde2058947755de
Aderbal Farias <aderbalfarias@hotmail.com>
2018-08-04 20:05:57 -0300

    Implementing yargs

--- notes.js
+++ notes.js
@@ -1,10 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
 };
 
-module.exports.add = (a, b) => {
-	return a + b;
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
